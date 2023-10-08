3d48e040643627ed86c671cc24b934bbf1ac6a86
Nitin Kumar Dey <105306697+nkdey14@users.noreply.github.com>
2023-10-08 23:26:41 +0530

    Used Constructor in src/components/News.js

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,20 +2,16 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description } = this.props;
+    let { title, description, imageUrl, newsUrl } = this.props;
     return (
-      <div>
+      <div className="container my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img
-            src="https://i0.wp.com/9to5mac.com/wp-content/uploads/sites/6/2018/12/instagram-buy-an-ipad.jpg?resize=1200%2C628&quality=82&strip=all&ssl=1"
-            className="card-img-top"
-            alt="..."
-          />
+          <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/" className="btn btn-primary">
-              Read the full story
+            <a href="/newsDetail" className="btn btn-sm btn-primary">
+              Read More
             </a>
           </div>
         </div>

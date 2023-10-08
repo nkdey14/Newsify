972e79d68b675b7b6e2a00040e767ea24d6b7e37
Nitin Kumar Dey <105306697+nkdey14@users.noreply.github.com>
2023-10-08 12:32:47 +0530

    Modified src/components/News.js and NewsItem.js

--- src/components/News.js
+++ src/components/News.js
@@ -4,9 +4,20 @@ import NewsItem from "./NewsItem";
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a News component
-        <NewsItem />
+      <div className="container my-3">
+        <h3>Newsify - Top Headlines</h3>
+        <br />
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="myDesc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="myDesc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="myDesc" />
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,7 +2,25 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    return <div>This is a NewItem component</div>;
+    let { title, description } = this.props;
+    return (
+      <div>
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src="https://i0.wp.com/9to5mac.com/wp-content/uploads/sites/6/2018/12/instagram-buy-an-ipad.jpg?resize=1200%2C628&quality=82&strip=all&ssl=1"
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/" className="btn btn-primary">
+              Read the full story
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
